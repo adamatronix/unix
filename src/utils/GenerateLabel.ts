0ea0800a4e83848e1da9a06b3c392e294deedc23
Your Name <you@example.com>
2021-12-10 11:54:49 +0800

    file Load in font and add to a canvas

--- src/utils/GenerateLabel.ts
+++ src/utils/GenerateLabel.ts
@@ -1,17 +1,25 @@
+import ScreenFont from 'url:../assets/fonts/Scr18.ttf';
+
 class GenerateLabel {
 
   constructor(label:string) {
-    this.writeTextToCanvas(label);
+    let screenFont = new FontFace('Screen', `url(${ScreenFont})`);
+    screenFont.load().then((font) => {
+      document.fonts.add(font);
+      this.writeTextToCanvas(label);
+    });
+    
   }
 
   writeTextToCanvas = (text:string) => {
     let canvas = document.createElement('canvas');
-    canvas.width = 1000;
-    canvas.height = 200;
-    document.body.appendChild(canvas);
+    canvas.width = 800;
+    canvas.height = 50;
     let context = canvas.getContext('2d');
-    context.font = "30px Arial";
-    context.fillText(text, 10, 50);
+    context.font = "50px Screen";
+    context.textAlign = "center";
+    context.fillStyle = "#ffffff";
+    context.fillText(text, 400, 50);
   }
 }
 
